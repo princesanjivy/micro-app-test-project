66b785db0bdf09a6dd5122e68b844b9ec04eccc3
Prince Sanjivy <34624548+princesanjivy@users.noreply.github.com>
2024-06-14 00:22:42 +0530

    test micro screen clear

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
+import time
+
 print('hello:', 3+19)
+
+for i in range(1, 11):
+  print(i)
+  input("Enter something: ")
+
+
+print("$<clear>") # to clear the screeen
+
+for i in range(2, 21):
+  print(i)
